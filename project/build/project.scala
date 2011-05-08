56320e2f7919d2e1525421e87296eb93218d18e8
Paul Butcher <paul@paulbutcher.com>
2011-05-08 19:27:14 +0100

    Add paid project

--- project/build/project.scala
+++ project/build/project.scala
@@ -8,7 +8,8 @@ class Parent(info: ProjectInfo) extends ParentProject(info) {
   override def updateAction = task { None }
 
   lazy val lib = project("library", "My Location Library", new LibraryProject(_))
-  lazy val trial  = project("trial", "My Location Trial", new MainProject(_), lib)
+  lazy val trial = project("trial", "My Location Trial", new MainProject(_), lib)
+  lazy val paid = project("paid", "My Location Paid", new MainProject(_), lib)
 
   class MainProject(info: ProjectInfo) extends AndroidProject(info) with Defaults
 
